637b8e37ff01ef975ffc2147bcba240b012bff31
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-05-29 11:54:45 -0400

    copied and understood express, jwt, middlewares, user auth from guide. added or modified 5 files

---
+++ config/db.js
@@ -0,0 +1,19 @@
+
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI = "mongodb://localhost/node-auth";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
